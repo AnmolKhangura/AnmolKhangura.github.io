6aea5326333e07c9b1082e1d5fdd5279c5681e51
Anmolpreet Khangura <109428188+AnmolKhangura@users.noreply.github.com>
2025-10-29 19:34:22 -0500

    fix nav title Home to About

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
       boxShadow: "none",
     }}>
       <Toolbar>
-        <Button color="primary" href="#about" disableRipple variant="nav">Home</Button>
+        <Button color="primary" href="#about" disableRipple variant="nav">About</Button>
         <Button color="primary" href="#experience" disableRipple variant="nav">Experience</Button>
         <Button color="primary" href="#projects" disableRipple variant="nav">Projects</Button>
         <Button color="primary" href="#contact" disableRipple variant="nav">Contact</Button>
